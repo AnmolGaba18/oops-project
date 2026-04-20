8e325837f4e4cad6a42ca2dd0d057b395773fe32
Anmol Gaba <gabaanmol45@gmail.com>
2026-04-20 21:47:24 +0530

    Update ResourceRepository.java

--- src/main/java/com/disaster/ResourceRepository.java
+++ src/main/java/com/disaster/ResourceRepository.java
@@ -1,3 +1,4 @@
+ubuntu@ip-172-31-2-162:~/oops-project$ cat src/main/java/com/disaster/ResourceRepository.java
 package com.disaster;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -5,11 +6,12 @@ import java.util.List;
 
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
 
+    List<Resource> findByNameContainingIgnoreCase(String name);
+
+    List<Resource> findByDisaster(String disaster);
+
     List<Resource> findByAvailable(boolean available);
 
-    List<Resource> findByTypeContainingIgnoreCase(String type);
+    List<Resource> findByType(String type);
 
-    List<Resource> findByDisasterTypeContainingIgnoreCase(String disasterType);
-
-    List<Resource> findByNameContainingIgnoreCase(String name);
 }
